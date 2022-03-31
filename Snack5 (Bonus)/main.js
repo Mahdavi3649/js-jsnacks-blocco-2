231661d8c8c6c11cfcfa87caab3788a29fe00d47
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-31 16:21:02 +0200

    Bonus Completato

--- Snack5 (Bonus)/main.js
+++ Snack5 (Bonus)/main.js
@@ -1,7 +1,6 @@
 /* Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
  */
 
-
 let numeri = [4,66,71,28,51,,33,19,7,23,14]
 
 let nomi = ["John","Mark","Sam","Michel","Sara","Maria","Dale"]
