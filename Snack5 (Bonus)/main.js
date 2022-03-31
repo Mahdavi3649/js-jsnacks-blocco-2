eed0b1074d541a99e159943ac077860611610bb8
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-31 16:29:27 +0200

    Bonus Completato

--- Snack5 (Bonus)/main.js
+++ Snack5 (Bonus)/main.js
@@ -1,17 +1,21 @@
 /* Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
  */
 
-let numeri = [4,66,71,28,51,,33,19,7,23,14]
+let numeri = [4,66,71,28,51,33,19,7,23,14]
 
 let nomi = ["John","Mark","Sam","Michel","Sara","Maria","Dale"]
+
+let addElement;
 
 while (numeri.length !== nomi.length) {
 
     if(numeri.length > nomi.length){
-        nomi.push("");
+        const addElement = prompt("add your element in array list")
+        nomi.push(addElement);
 
     }else if(nomi.length > numeri.length){
-        numeri.push("");
+        nomi.push(addElement);
+
     }
     console.log(numeri,nomi);  
 }
